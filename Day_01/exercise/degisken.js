0cde39292714e7b9bd1240dec2b62c8a3c927bd8
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2022-12-12 15:40:05 +0300

    Day01 exercise completed. 🚀

---
+++ Day_01/exercise/degisken.js
@@ -0,0 +1,4 @@
+let isim = "Erdem";
+let dogruMu = true;
+let deneme = undefined;
+let bos = null;
---
+++ Day_01/exercise/exercise.js
@@ -0,0 +1,49 @@
+/*
+    Görevler
+        + 1- "Yorumlar kodu okunabilir hale getirmeye yardımcı olur" yazan tek satırlı bir yorum yazın
+        + 2- "30GundeJavaScript eğitimine hoş geldiniz" yazan başka bir yorum satırı oluşturun
+        + 3- "Yorumlar kodu okunabilir, yeniden kullanımı kolay ve bilgilendirici bir hale getirir." yazılı, birden çok satıra yayılmış çok satırlı bir yorum yazın
+        + 4- Bir degisken.js isimli dosya oluşturun ve bu .js dosyasında string, boolean, undefined ve null veri türlerinde değişkenler oluşturun
+        + 5- Bir veriturleri.js isimli dosya oluşturun farklı veri türlerindeki verileri kontrol etmek için JavaScript typeof operatörünü kullanın.
+        + 6- Herhangi bir değer ataması yapmadan dört adet değişken yazın
+        + 7- Oluşturduğunuz değişkenlere değer ataması yapın
+        + 8- Adınızı, soyadınızı, medeni durumunuzu, ülkenizi ve yaşınızı birden çok satırda olacak şekilde saklamak için değişkenleri ve değerlerini oluşturun
+        + 9- Adınızı, soyadınızı, medeni durumunuzu, ülkenizi ve yaşınızı tek bir satırda olacak şekilde saklamak için değişkenleri ve değerlerini oluşturun
+        + 10- benimYasim ve seninYasin adlı iki değişken oluşturun ve bunlara aşağıdaki değerleri atayın. Ardından bunları tarayıcının konsolunda çalıştırın
+*/
+
+// Yorumlar kodu okunabilir hale getirmeye yardımcı olur
+// 30GundeJavaScript eğitimine hoş geldiniz
+/*
+    Yorumlar kodu okunabilir, 
+    yeniden kullanımı kolay ve bilgilendirici 
+    bir hale getirir.
+*/
+
+let metin1;
+let metin2;
+let sayi1;
+let sayi2;
+
+metin1 = "Merhaba";
+metin2 = "Nasılsın?";
+sayi1 = 19;
+sayi2 = 112;
+
+let ad = "Erdem Alptuğ";
+let soyAd = "Öksüz";
+let medeniDurum = false;
+let ulke = "Türkiye";
+let yas = 19;
+
+let ad1 = "Erdem Alptuğ",
+  soyAd1 = "Öksüz",
+  medeniDurum1 = false,
+  ulke1 = "Türkiye",
+  yas1 = 19;
+
+let benimYasim = "Ben 25 Yaşındayım.";
+let seninYasin = "Sen 30 Yaşındasın.";
+
+console.log(benimYasim);
+console.log(seninYasin);
